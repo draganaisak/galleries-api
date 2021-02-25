a3644ef8acfe701088ae3cabd56ee56501704792
draganaisak <dragana.isak@gmail.com>
2021-02-25 15:07:01 +0100

    added UpdateGalleryRequest

---
+++ app/Http/Requests/UpdateGalleryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateGalleryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|string|min:2|max:255',
+            'description' => 'string|max:1000',
+            'images' => 'sometimes|array|min:1',
+            'images.*.url' => 'sometimes|URL|ends_with:.png,.jpeg,.jpg'
+        ];
+    }
+}
